933cffed472d77898f4e2872d799cf96b09954ab
Exgc <1196140338@qq.com>
2020-06-23 00:24:45 +0800

    item返回数据增加了type表内容、增加了tag属性

--- demo/src/main/java/com/example/demo/dao/ItemDao.java
+++ demo/src/main/java/com/example/demo/dao/ItemDao.java
@@ -1,5 +1,6 @@
 package com.example.demo.dao;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.entity.Item;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -14,8 +15,10 @@ public interface ItemDao {
     @Insert("INSERT INTO item(`itemInfo`,`itemLocation`,`status`,`itemImg1`,`itemImg2`,`itemImg3`,`itemImg4`,`telephone`,`sellerId`,`type`) values(#{itemInfo},#{itemLocation},-1,#{itemImg1},#{itemImg2},#{itemImg3},#{itemImg4},#{telephoneNumber},#{sellerId},#{type});")
     Integer addItem(Item item);
 
-    @Select("SELECT * FROM item WHERE itemId=#{itemId};")
-    Item getItemById(int itemId);
+    @Select("select * from (item left join type1 on item.itemId=type1.itemId) left join type2 on item.itemId=type2.itemId where item.itemId=#{itemId}\n" +
+            "union\n" +
+            "select * from (item right join type1 on item.itemId=type1.itemId) right join type2 on item.itemId=type2.itemId where item.itemId=#{itemId};")
+    JSONObject getItemById(int itemId);
 
     @Update("UPDATE item SET itemInfo=#{itemInfo},itemLocation=#{itemLocation},itemImg1=#{itemImg1},itemImg2=#{itemImg2},itemImg3=#{itemImg3},itemImg4=#{itemImg4},telephone=#{telephoneNumber} where itemId=#{itemId};")
     Integer updateItem(Item item);
@@ -23,6 +26,8 @@ public interface ItemDao {
     @Update("UPDATE item SET STATUS=2 WHERE itemId=#{itemId};")
     Integer deleteItemById(int itemId);
 
-    @Select("SELECT * FROM item where status !=2")
-    List<Item> getItemList();
+    @Select("select * from (item left join type1 on item.itemId=type1.itemId) left join type2 on item.itemId=type2.itemId where item.status in (-1,0,1)\n" +
+            "union\n" +
+            "select * from (item right join type1 on item.itemId=type1.itemId) right join type2 on item.itemId=type2.itemId where item.status in (-1,0,1);")
+    List<JSONObject> getItemList();
 }
