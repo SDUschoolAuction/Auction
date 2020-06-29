06ac5fe4077f9fb2af7a018258a4f109dafb65f5
Exgc <1196140338@qq.com>
2020-06-29 19:05:00 +0800

    cxz

--- Front-end/pages/public/public.js
+++ Front-end/pages/public/public.js
@@ -290,6 +290,7 @@ Page({
                 'content-type': 'application/json'
               },
               success: function(res) {
+                console.log("cxz");
                 console.log(res);
                 
               },
